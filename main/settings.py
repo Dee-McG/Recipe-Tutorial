9751c3c307cc2d72775254fee2855d7370b1b56a
Daisy McGirr <mcgirr.daisy@gmail.com>
2023-03-20 01:48:08 +0000

    Removed duplicated context_processor

--- main/settings.py
+++ main/settings.py
@@ -85,7 +85,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                 'django.template.context_processors.request',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
