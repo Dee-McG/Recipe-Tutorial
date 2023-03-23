482711edc3ff4517590360276c44d133d3e1774e
Daisy McGirr <mcgirr.daisy@gmail.com>
2023-03-23 01:31:58 +0000

    Edit recipe functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import AddRecipe, Recipes, RecipeDetail, DeleteRecipe
+from .views import (
+    AddRecipe, Recipes,
+    RecipeDetail, DeleteRecipe,
+    EditRecipe
+)
 
 
 urlpatterns = [
@@ -7,4 +11,5 @@ urlpatterns = [
     path("", Recipes.as_view(), name="recipes"),
     path("<slug:pk>/", RecipeDetail.as_view(), name="recipe_detail"),
     path("delete/<slug:pk>/", DeleteRecipe.as_view(), name="delete_recipe"),
+    path("edit/<slug:pk>/", EditRecipe.as_view(), name="edit_recipe",)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import (
     CreateView, ListView,
-    DetailView, DeleteView
+    DetailView, DeleteView,
+    UpdateView
 )
 
 from django.contrib.auth.mixins import (
@@ -40,6 +41,17 @@ class AddRecipe(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddRecipe, self).form_valid(form)
+
+
+class EditRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Edit a recipe"""
+    template_name = 'recipes/edit_recipe.html'
+    model = Recipe
+    form_class = RecipeForm
+    success_url = '/recipes/'
+    
+    def test_func(self):
+        return self.request.user == self.get_object().user
 
 
 class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
