02de32c22f12f8e16c5145d9ed3ee05bf9dbfa0f
Daisy McGirr <mcgirr.daisy@gmail.com>
2023-03-23 02:15:59 +0000

    Home page styling and content

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from recipes.models import Recipe
 
 
-class Index(TemplateView):
+class Index(ListView):
     template_name = 'home/index.html'
+    model = Recipe
+    context_object_name = 'recipes'
+
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
