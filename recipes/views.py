044bae84855451c7010f6e6808ae769756ff1f36
Daisy McGirr <mcgirr.daisy@gmail.com>
2023-03-23 01:52:22 +0000

    Add search functionality

--- recipes/views.py
+++ recipes/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.mixins import (
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.db.models import Q
+
 from .models import Recipe
 from .forms import RecipeForm
 
@@ -20,6 +22,20 @@ class Recipes(ListView):
     template_name = "recipes/recipes.html"
     model = Recipe
     context_object_name = "recipes"
+
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get('q')
+        if query:
+            recipes = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query) |
+                Q(instructions__icontains=query) |
+                Q(ingredients__icontains=query) |
+                Q(cuisine_type__icontains=query)
+            )
+        else:
+            recipes = self.model.objects.all()
+        return recipes
 
 
 class RecipeDetail(DetailView):
